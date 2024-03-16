a02ba80f400bb06b20a0136c623cdd13f9033c35
Jakub Staniszewski <kuba.staniszewski.ks@gmail.com>
2024-03-16 16:04:55 +0100

    create enter nick screen

---
+++ frontend/tictactoepvp/src/screens/EnterNick/EnterNick.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NickInput from '../../components/NickInput/NickInput.tsx'
+
+function EnterNick() {
+  return (
+    <div>
+        <div>EnterNick</div>
+        <NickInput />
+    </div>
+  )
+}
+
+export default EnterNick
