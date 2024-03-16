5fa345fb85a833d309f2650d7bda198f6a26ebe0
Jakub Staniszewski <kuba.staniszewski.ks@gmail.com>
2024-03-16 16:01:00 +0100

    create game tile

---
+++ frontend/tictactoepvp/src/components/GameTile/GameTile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GameTile.css'
+
+function GameTile() {
+  return (
+    <div className='gt-game-tile'>
+        <h1 className='gt-value'>x</h1>
+    </div>
+  )
+}
+
+export default GameTile
