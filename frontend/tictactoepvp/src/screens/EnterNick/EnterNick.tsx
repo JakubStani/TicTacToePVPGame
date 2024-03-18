4e694b487ff30c59226ba2f65cef8f8184666381
Jakub <kuba.staniszewski.ks@gmail.com>
2024-03-18 20:21:34 +0100

    add more content to enter nick

--- frontend/tictactoepvp/src/screens/EnterNick/EnterNick.tsx
+++ frontend/tictactoepvp/src/screens/EnterNick/EnterNick.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NickInput from '../../components/NickInput/NickInput.tsx'
 
 function EnterNick() {
+  const [nick, setUserNick] = useState('');
+
   return (
     <div>
-        <div>EnterNick</div>
-        <NickInput />
+      {nick==='' &&
+        <div>
+          <div>EnterNick</div>
+          <NickInput nick={nick} setNick={setUserNick}/>
+        </div>
+      }
+      {nick !=='' &&
+      <div>
+        <div>
+          <h1>Hello {nick}!</h1>
+          <p>Searching for an opponent</p>
+        </div>
+        {/*TODO: insert loading animation */}
+        <h1>Loading...</h1>
+      </div>
+
+      }
     </div>
   )
 }
