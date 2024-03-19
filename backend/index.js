aecfb80bd73a352daea383135b270f306c032fd3
Jakub <kuba.staniszewski.ks@gmail.com>
2024-03-19 18:55:24 +0100

    prepare server

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const {WebSocketServer} = require('ws')
 
 const url = require('url')
-const uuidv4 = require('uuid'.v4)
+const uuidv4 = require('uuid').v4
 
 const server = http.createServer()
 
@@ -16,9 +16,49 @@ const playersInQueue = []
 
 const handleMessage = (bytes, uuid) => {
 
+    const message = JSON.parse(bytes.toString())
+    const user = users[uuid]
+
+    console.log(message)
+
+    //here make switch that depend on a message
+    //do not forgot about notifyPlayers()
 }
 
 const handleClose = (uuid) => {
+    const user = users[uuid]
+    const closedGame = gameboards[user['gameUuid']]
+    
+    //not only delete user, but end game too
+    if(user['userState'] === 'playing') {
+
+        let user2=null
+
+        //end game
+        if(closedGame['cross'] === uuid) {
+            user2 = closedGame['circle']
+        } else {
+            user2 = closedGame['cross']
+        }
+
+        user2['userState'] = 'endedGame'
+        user2['gameUuid'] = ''
+
+        delete gameboards[user['gameUuid']]
+    }
+    //delete user from queue
+    else {
+        if(user['userState'] !== 'endedGame') {
+            delete playersInQueue[uuid]
+        }
+    }
+
+    //delete user
+    delete connections[uuid]
+    delete users[uuid]
+
+    console.log(`User ${user['username']} has disconnected`)
+    console.log(`Users left: ${JSON.stringify(users)}`)
 
 }
 
@@ -32,11 +72,28 @@ const handleQueue = (uuid) => {
     }
 }
 
+const notifyPlayers = () => {
+    Object.keys(gameboards).forEach(gameData => {
+        const connection1 = connections[gameData['cross']]
+        const connection2 = connections[gameData['circle']]
+        const gameState = JSON.stringify(gameData['state'])
+
+        connection1.send(gameState)
+        connection2.send(gameState)
+    })
+}
+
 const createAGame = (uuid1, uuid2) => {
-    gameboards[`${uuid1}${uuid2}`] = {
+    const gameUuid = `${uuid1}${uuid2}`
+    gameboards[gameUuid] = {
         cross: uuid1,
         circle: uuid2, 
         state: ['', '', '', '', '', '', '', '', '']}
+    users[uuid1]['userState'] = 'playing'
+    users[uuid1]['gameUuid'] = gameUuid
+
+    users[uuid2]['userState'] = 'playing'
+    users[uuid2]['gameUuid'] = gameUuid
 }
 
 wsServer.on('connection', (connection, request) => {
@@ -48,7 +105,9 @@ wsServer.on('connection', (connection, request) => {
 
     connections[uuid] = connection
     users[uuid] = {
-        username: username
+        username: username,
+        userState: 'waiting',
+        gameUuid: ''
     }
 
     connection.on('message', message => handleMessage(message, uuid))
