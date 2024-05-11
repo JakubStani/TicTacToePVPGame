1b0b88f928911e62c6fcf21fbce2bb62b0c38e65
Jakub <kuba.staniszewski.ks@gmail.com>
2024-05-11 18:32:58 +0200

    allow to sign in

--- frontend/tictactoepvp/src/screens/SignIn/SignIn.js
+++ frontend/tictactoepvp/src/screens/SignIn/SignIn.js
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 import "./SignIn.css";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "../../auth/UserPool";
 
 function SignIn(props) {
-  const [email, setEmail] = useState("");
+  const [nick, setNick] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
+
+    const user = new CognitoUser({
+      Username: nick,
+      Pool: UserPool,
+    });
+
+    const authDetails = new AuthenticationDetails({
+      Username: nick,
+      Password: password,
+    });
+
+    user.authenticateUser(authDetails, {
+      onSuccess: (data) => {
+        console.log("Success ", data);
+        console.log(data.getIdToken().getJwtToken());
+        console.log(data.getAccessToken().getJwtToken());
+        console.log(data.getRefreshToken().getToken());
+        //localStorage.setItem('accessToken-tttpvp', data[])
+      },
+      onFailure: (error) => {
+        console.error("Error ", error);
+      },
+    });
 
     props.setIsAuthenticated(true);
     props.setShowSignIn(true);
@@ -16,11 +41,11 @@ function SignIn(props) {
     <div>
       <div>Sign In</div>
       <form onSubmit={onSubmit} className="si-auth-container">
-        <label>Email</label>
+        <label>Nick</label>
         <input
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          type="text"
+          value={nick}
+          onChange={(event) => setNick(event.target.value)}
         />
 
         <label>Password</label>
